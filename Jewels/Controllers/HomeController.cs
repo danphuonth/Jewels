57063f96a6a78dc385e80645d6dba8a0dd158583
Dang Phuong Thao <19dh111030@st.huflit.edu.vn>
2023-02-20 13:06:14 +0700

    Add project files.

---
+++ Jewels/Controllers/HomeController.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Jewels.DAL;
+using Jewels.Models;
+using PagedList;
+
+namespace Jewels.Controllers
+{
+    public class HomeController : Controller
+    {
+        private JewelsContext db = new JewelsContext();
+        public ActionResult Index()
+        {
+            var sanPhams = db.SanPhams.Include(s => s.ChatLieuSP).Include(s => s.LoaiSP);
+            return View(sanPhams.ToList());
+
+        }
+        public ActionResult Search(int? page)
+        {
+            var pageNumber = page == null || page <= 0 ? 1 : page.Value;
+            var pageSize = 12;
+            var IsSanPhams = db.SanPhams.AsNoTracking()
+                .Include(s => s.ChatLieuSP)
+                .Include(s => s.LoaiSP)
+                .OrderByDescending(x => x.SanPhamID); // sắp xếp
+            PagedList<SanPham> models = new PagedList<SanPham>(IsSanPhams, pageNumber, pageSize);
+            ViewBag.CurrentPage = pageNumber;
+
+            return View(models);
+        }
+        
+        public ActionResult TraCuu(string searchString)
+        {
+            List<TrangThaiDH> trangThaiDHs = db.TrangThaiDHs.ToList();
+            List<HinhThucTT> hinhThucTTs = db.HinhThucTTs.ToList();
+            IEnumerable<DonHang> items = db.DonHangs.Include(d => d.KhuyenMai).Include(d => d.TrangThaiDH).Include(d => d.HinhThucTT).OrderByDescending(x => x.DonHangID);
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                items = db.DonHangs.Where(x => x.DienThoai.Contains(searchString));
+                return View(items);
+            }
+            else
+            {
+                ViewBag.Fail = "Số điện thoại không tồn tại mời bạn nhập lại ";
+                return View();
+            }
+          
+
+
+        }
+        public ActionResult KhuyenMai()
+        {
+            return View(db.KhuyenMais.ToList());
+        }
+     
+        /* Tao phan hoi */
+        public ActionResult Contact()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Contact([Bind(Include = "PhanHoiID,TenKH,MucDoHaiLong,MoTa")] PhanHoiKH phanHoiKH)
+        {
+            if (ModelState.IsValid)
+            {
+                db.PhanHoiKHs.Add(phanHoiKH);
+                db.SaveChanges();
+                return RedirectToAction("Contact");
+            }
+
+            return View(phanHoiKH);
+        }
+        /*XEM THEM  san pham MOI HOT BanChay */
+        public ActionResult spMoi()
+        {
+            var spMoi = db.SanPhams.OrderByDescending(x => x.SanPhamID).Where(x => x.Moi == "Yes").ToList();
+            return View(spMoi);
+        }
+        public ActionResult spBanChay()
+        {
+            var spBanChay = db.SanPhams.OrderByDescending(x => x.SanPhamID).Where(x => x.BanChay == "Yes").ToList();
+            return View(spBanChay);
+        }
+        public ActionResult spHot()
+        {
+            var spHot = db.SanPhams.AsNoTracking().OrderByDescending(x => x.SanPhamID).Where(x => x.Hot == "Yes").ToList();
+            return View(spHot);
+        }
+        /* Danh muc san pham */
+        public ActionResult Nhan(int? page)
+        {
+            var pageNumber = page == null || page <= 0 ? 1 : page.Value;
+            var pageSize = 8;
+            var IsSanPhams = db.SanPhams.AsNoTracking()
+                    .OrderByDescending(x => x.SanPhamID).Where(x => x.LoaiSPID == 1).ToList();
+            PagedList<SanPham> models = new PagedList<SanPham>(IsSanPhams, pageNumber, pageSize);
+            ViewBag.CurrentPage = pageNumber;
+
+            return View(models);
+
+        }
+        public ActionResult VongTay(int? page)
+        {
+            var pageNumber = page == null || page <= 0 ? 1 : page.Value;
+            var pageSize = 8;
+            var IsSanPhams = db.SanPhams.AsNoTracking()
+                    .OrderByDescending(x => x.SanPhamID).Where(x => x.LoaiSPID == 2).ToList();
+            PagedList<SanPham> models = new PagedList<SanPham>(IsSanPhams, pageNumber, pageSize);
+            ViewBag.CurrentPage = pageNumber;
+
+            return View(models);
+
+        }
+
+        public ActionResult KhuyenTai(int? page)
+        {
+            var pageNumber = page == null || page <= 0 ? 1 : page.Value;
+            var pageSize = 8;
+            var IsSanPhams = db.SanPhams.AsNoTracking()
+                    .OrderByDescending(x => x.SanPhamID).Where(x => x.LoaiSPID == 3).ToList();
+            PagedList<SanPham> models = new PagedList<SanPham>(IsSanPhams, pageNumber, pageSize);
+            ViewBag.CurrentPage = pageNumber;
+
+            return View(models);
+
+        }
+
+        public ActionResult DayChuyen(int? page)
+        {
+            var pageNumber = page == null || page <= 0 ? 1 : page.Value;
+            var pageSize = 8;
+            var IsSanPhams = db.SanPhams.AsNoTracking()
+                    .OrderByDescending(x => x.SanPhamID).Where(x => x.LoaiSPID == 4).ToList();
+            PagedList<SanPham> models = new PagedList<SanPham>(IsSanPhams, pageNumber, pageSize);
+            ViewBag.CurrentPage = pageNumber;
+
+            return View(models);
+
+        }
+    }
+}
---
+++ Jewels/Controllers/UserController.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Jewels.Models;
+using Jewels.DAL;
+
+namespace Jewels.Controllers
+{
+    public class UserController : Controller
+    {
+        private JewelsContext db = new JewelsContext();
+        // GET: KhachHangs/Create
+        public ActionResult DangKy()
+        {
+            return View();
+        }
+
+        // POST: KhachHangs/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult DangKy([Bind(Include = "KhachHangID,TenKH,TichLuy,DienThoai,Email,DiaChi,MatKhau")] KhachHang khachHang)
+        {
+
+            try
+            {
+                khachHang.TichLuy = 0;
+                // Thêm người dùng  mới
+                db.KhachHangs.Add(khachHang);
+                // Lưu lại vào cơ sở dữ liệu
+                db.SaveChanges();
+                // Nếu dữ liệu đúng thì trả về trang đăng nhập
+                if (ModelState.IsValid)
+                {
+                    return RedirectToAction("DangNhap");
+                }
+                return View("Dangky");
+
+            }
+            catch
+            {
+                return View();
+            }
+
+        }
+
+        public ActionResult DangNhap()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult DangNhap(string DienThoai, string MatKhau)
+        {
+            var user = db.KhachHangs.SingleOrDefault(x => x.DienThoai.Equals(DienThoai) && x.MatKhau.Equals(MatKhau));
+            if (user != null)
+            {
+                if (user.DienThoai == "Ad6412")
+                {
+
+                    Session["admin"] = user;
+                    return RedirectToAction("Index", "DonHangs");
+                }
+                else
+                {
+                    Session["TenKH"] = user.TenKH;
+                    Session["userId"] = user.KhachHangID;
+                    Session["user"] = user;
+
+                    return RedirectToAction("ShowToCart", "ShoppingCart");
+                }
+            }
+            else
+            {
+                ViewBag.Fail = "Đăng nhập thất bại";
+                return View("DangNhap");
+            }
+
+        }
+        public ActionResult Profile()
+        {
+            if (Session["user"] == null)
+            {
+                return RedirectToAction("DangNhap", "User");
+
+            }
+            else
+            {
+                var u = Session["user"] as Jewels.Models.KhachHang;
+                KhachHang khachHang = db.KhachHangs.Find(u.KhachHangID);
+                return View(khachHang);
+
+            }
+        }
+        public ActionResult DangXuat()
+        {
+
+            Session.Clear();
+            return RedirectToAction("Index", "Home");
+
+        }
+
+    }
+}
